954f21d3286ef1da7cf9911065ea592583da256c
葛泰麟 <1934036260@qq.com>
2020-08-03 13:28:41 +0800

    1 删掉原内容 移入新项目
    2020年8月3日13:28:38

---
+++ Assets/Demo1/Scripts/Demo1Context.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using strange.extensions.context.impl;
+using UnityEngine;
+
+
+
+/// <summary>
+/// 上下文内容
+/// </summary>
+public class Demo1Context : MVCSContext
+{
+    
+}
---
+++ Assets/Demo1/Scripts/Demo1ContextView.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using strange.extensions.context.impl;
+using UnityEngine;
+
+/// <summary>
+/// root  启动入口
+/// </summary>
+public class Demo1ContextView : ContextView
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
